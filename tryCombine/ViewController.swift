953daae3c15e84714fae149e78c1081d674958fd
Dawid Czmyr <dczmyr@bluesoft.net.pl>
2019-10-16 00:29:12 +0200

    setup view and properties

--- tryCombine/ViewController.swift
+++ tryCombine/ViewController.swift
@@ -11,6 +11,9 @@ import Combine
 import PureLayout
 
 class ViewController: UIViewController {
+    private var buttonSwitchValue: Bool = false
+    private var switchSubscriber: AnyCancellable?
+
     private let barButton: UIBarButtonItem = {
         let button = UIBarButtonItem()
         let largeConfig = UIImage.SymbolConfiguration(weight: .bold)
@@ -20,18 +23,38 @@ class ViewController: UIViewController {
         )
 
         button.image = systemImage
-        button.tintColor = .gray
+        button.tintColor = .black
 
         return button
+    }()
+
+    private let switchButon: UISwitch = UISwitch()
+    private let switchMeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "switch me"
+
+        return label
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationItem.rightBarButtonItem = self.barButton
-
+        self.navigationItem.rightBarButtonItem?.isEnabled = false
+        self.addSubviews()
+        self.addConstraints()
     }
 
+    fileprivate func addSubviews() {
+        self.view.addSubview(self.switchMeLabel)
+        self.view.addSubview(self.switchButon)
+    }
 
+    fileprivate func addConstraints() {
+        self.switchMeLabel.autoPinEdge(toSuperviewEdge: .top, withInset: 100)
+        self.switchMeLabel.autoPinEdge(toSuperviewEdge: .left, withInset: 16)
+        self.switchButon.autoPinEdge(.left, to: .right, of: self.switchMeLabel, withOffset: 11)
+        self.switchButon.autoAlignAxis(.horizontal, toSameAxisOf: self.switchMeLabel)
+    }
 }
 
